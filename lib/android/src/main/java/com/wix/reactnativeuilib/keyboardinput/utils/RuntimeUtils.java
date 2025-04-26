15e77cd0149ef3612f9aa6b629cbec5dec6c0958
Bin Be Bong <bin@gm.co>
2025-04-27 00:22:38 +0700

    update react native 0.79.1

--- lib/android/src/main/java/com/wix/reactnativeuilib/keyboardinput/utils/RuntimeUtils.java
+++ lib/android/src/main/java/com/wix/reactnativeuilib/keyboardinput/utils/RuntimeUtils.java
@@ -6,10 +6,27 @@ import com.wix.reactnativeuilib.keyboardinput.ReactContextHolder;
 public class RuntimeUtils {
 
     // TODO Switch to GuardedRunnable when upgrading RN's minimal ver
+    // private static final Runnable sUIUpdateClosure = new Runnable() {
+    //     @Override
+    //     public void run() {
+    //         ReactContextHolder.getContext().getNativeModule(UIManagerModule.class).onBatchComplete();
+    //     }
+    // };
     private static final Runnable sUIUpdateClosure = new Runnable() {
         @Override
         public void run() {
-            ReactContextHolder.getContext().getNativeModule(UIManagerModule.class).onBatchComplete();
+            try {
+                UIManagerModule uiManager = ReactContextHolder.getContext()
+                        .getNativeModule(UIManagerModule.class);
+                if (uiManager != null) {
+                    uiManager.onBatchComplete();
+                } else {
+                    // Log the error or handle it accordingly
+                    System.err.println("UIManagerModule is null, cannot call onBatchComplete");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     };
 
